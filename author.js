4af3c62738e4afb61e4be3ebc114af80ebc6b3b6
Akashe <160213887+AkasheDev@users.noreply.github.com>
2025-12-28 14:13:14 +0300

    Add author signature to DOM on load

---
+++ author.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var signature = document.getElementById("akashe-signature");
+    if (signature) {
+        signature.innerHTML = ' | <a href="https://akashe.dev" target="_blank" title="Web Developer" style="text-decoration:none; font-weight:bold; color:inherit;">Designed by Akashe Dev</a>';
+    }
+});
