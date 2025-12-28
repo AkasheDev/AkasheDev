4a7f6c69402543fadcc39a145e5bcb569306785b
Akashe <160213887+AkasheDev@users.noreply.github.com>
2025-12-28 14:39:15 +0300

    Delete author.js

--- author.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var signature = document.getElementById("akashe-author");
-    if (signature) {
-        signature.innerHTML = ' | <a href="https://akashe.dev" target="_blank" title="Web Developer" style="text-decoration:none; font-weight:bold; color:inherit;">Designed by Akashe Dev</a>';
-    }
-});
